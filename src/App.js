6d9b1e128140ac98a51344f02c46d445ed1332aa
João Pedro Batista Santos <73478823+pepermao@users.noreply.github.com>
2022-05-03 20:59:26 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1
+        >
+          Hello word
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
